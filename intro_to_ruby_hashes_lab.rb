43672851ab241d8d7d27f65ef81e5e29829daedd
Kim Pham <pham.kim.a@gmail.com>
2018-08-23 19:10:01 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,21 +22,11 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-	monopoly = {}
-  monopoly[:railroads] = {}
-  monopoly[:railroads][:pieces] = 4
-  monopoly[:railroads][:names] = {}
-    :reading_railroad => {},
-    :pensylvania_railroad => {},
-    :b_and_o_railroad => {},
-    :shortline => {}
-  }
-  monopoly[:railroads][:rent_in_dollars] = {
-    :one_piece_owned => 25, 
-    :two_pieces_owned => 50, 
-    :three_pieces_owned => 100,
-    :four_pieces_owned => 200
-  }
+	monopoly = {
+	  :railroads => {}
+	  
+	}
+  
   monopoly
 end
 
